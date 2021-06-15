26e04cc14088ae6ce9d1fe741bc18cf07ca46371
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-15 20:50:28 +0100

    Remove react defaults in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="Home">
+      <header className="HomeHeader">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="GitHubLink"
+          href="https://github.com/IzerOnadim"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my GitHub
         </a>
       </header>
     </div>
