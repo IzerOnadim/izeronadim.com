fb3b5bf076fce78ae98379c1a77d433436e32c2b
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-17 20:18:22 +0100

    Remove unneeded comment

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,9 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Components/Home';
-import NavBar from './Components/NavBar/NavBar';
 
 function AppRouter() {
   return (
     <Router>
-      {/* <NavBar /> */}
       <Route exact path='/' component={Home} />
     </Router>
   );
