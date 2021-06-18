5b3f61735e7c5b4fb879d749e016181a00cb9c4b
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-18 23:07:09 +0100

    Further customise fonts and button dimensions

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,8 @@ import Button from '@material-ui/core/Button';
 
 function Home() {
   return (
-    <div className="Home">
-      <h1>IZER ONADIM</h1>
+    <div className='Home'>
+      <h1 className='HomeTitle'>IZER ONADIM</h1>
       <p className='aboutMe'>I'm a software engineer and computer science student at Imperial College London.</p>
       <LinkBar />
       <a className='ProjectsButton' href='/projects'> 
