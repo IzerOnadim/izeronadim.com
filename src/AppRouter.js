4b9d172c1ff933cb30524c536f9b21dbccdaf397
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-19 21:35:35 +0100

    Add route to portfolio page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Components/Home';
+import Portfolio from './Components/Portfolio/Portfolio';
 
 function AppRouter() {
   return (
     <Router>
       <Route exact path='/' component={Home} />
+      <Route exact path='/portfolio' component={Portfolio} />
     </Router>
   );
 }
