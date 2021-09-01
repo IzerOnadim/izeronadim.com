28642bed242f226dea328e6d682379294e987948
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-09-01 20:06:03 +0100

    Add under development banner

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,17 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
 import Home from './components/Home';
+import About from './components/about/About';
 import Portfolio from './components/portfolio/Portfolio';
+import UnderDev from './components/general/UnderDev';
 
 function AppRouter() {
   return (
     <Router>
+      <NavBar />
+      <UnderDev />
       <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
       <Route exact path='/portfolio' component={Portfolio} />
     </Router>
   );
---
+++ src/components/general/UnderDev.js
@@ -0,0 +1,11 @@
+import './UnderDev.css';
+
+function UnderDev() {
+  return (
+    <div className="UnderDev">
+      <p className="UnderDevText">This website is still under development. Thank you for your patience. </p>
+    </div>
+  );
+}
+
+export default UnderDev;
