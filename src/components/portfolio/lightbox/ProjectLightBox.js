9549e1d806fdd28ad5f2fd8cbb1070d90ea98303
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-09-01 20:39:12 +0100

    Add basic skeleton for ProjectLightBox

---
+++ src/components/portfolio/lightbox/ProjectLightBox.js
@@ -0,0 +1,12 @@
+import './ProjectLightBox.css';
+
+function ProjectLightBox() {
+  return (
+    <div className='LightBox'>
+      <h2>Project title</h2>
+      <p>Project Description</p>
+    </div>
+  );
+}
+
+export default ProjectLightBox;
